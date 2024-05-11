df1e9e767e4bb146521b4798f050fb7f4e0214a3
Madinakhx <madinakhx1995@gmail.com>
2024-05-11 15:52:38 +0500

    rty

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -4,8 +4,8 @@ function Navbar(props) {
     return (
         <div className="navbar">
             <div className="logo">
-                <a href="#"> nav</a>
-                <h1>Lorem ipsum dolor sit.</h1>
+                <a href="#"> Logo </a>
+                <h1 style={{background:"black", color:"gold"}}> Navbar component </h1>
             </div>
 
         </div>
